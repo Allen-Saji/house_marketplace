c74f1b6fb0f33eb53ecd44ecb91c42b98e0039df
Allen Saji <allensaji@gmail.com>
2022-09-06 12:26:09 +0530

    add listing route

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
+import Listing from './pages/Listing';
 
 
 function App() {
@@ -25,6 +26,7 @@ function App() {
               <Route path='/profile' element={<Profile />} />
           </Route>
           <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/category/:categoryName/:listingId' element={<Listing />} />
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/create-listing' element={<CreateListing />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
